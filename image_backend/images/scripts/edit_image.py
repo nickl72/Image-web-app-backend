35eb02c1182173263d43e3208a4c630ece7bd604
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-11 12:31:35 -0500

    can upload edited file to database, still need to create unique file names

--- image_backend/images/scripts/edit_image.py
+++ image_backend/images/scripts/edit_image.py
@@ -1,16 +1,20 @@
 # This file will process all requests to edit images
 import PIL 
+from ..models import User, Image as Img
 
 image_path = 'image_db'
 def open_image(image):
     return PIL.Image.open(f'{image_path}/{image.path}')
 
-# image is a PIL.Image
-def save_image(image):
-    image.save(f'{image_path}/new.jpg')
-    image.close()
-    return
+# new_image is a PIL.Image, original_image is a imageField_object
+def save_image(new_image, original_image):
+    # print(new_image.path)
+    new_image.save(f'{image_path}/temp/new.jpg')
+    
 
+    img = Img(title=original_image.title, path=f'{image_path}/temp/new.jpg', description=original_image.description, edited=True, creator=User.objects.filter(id=1)[0])
+    img.save()
+    return
 
 # image is a django database imageField object
 def brightness(image,value):
@@ -20,8 +24,9 @@ def blur(image, value):
     image = open_image(image)
     return
 def color(image, r=0, g=0, b=0):
+    print(image.title)
     img = open_image(image)
-    save_image(img)
+    save_image(img, image)
     print('image: ', image)
     print('r: ', r)
     print('g: ', g)
