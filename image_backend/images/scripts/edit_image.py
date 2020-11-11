5951148e9aef1aa65f0d7a1fd7191825c150e84e
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-11 11:51:19 -0500

    Edited images can be saved to image_db directory

--- image_backend/images/scripts/edit_image.py
+++ image_backend/images/scripts/edit_image.py
@@ -5,13 +5,23 @@ image_path = 'image_db'
 def open_image(image):
     return PIL.Image.open(f'{image_path}/{image.path}')
 
+# image is a PIL.Image
+def save_image(image):
+    image.save(f'{image_path}/new.jpg')
+    image.close()
+    return
 
+
+# image is a django database imageField object
 def brightness(image,value):
+    image = open_image(image)
     return
 def blur(image, value):
+    image = open_image(image)
     return
 def color(image, r=0, g=0, b=0):
-    image = open_image(image)
+    img = open_image(image)
+    save_image(img)
     print('image: ', image)
     print('r: ', r)
     print('g: ', g)
