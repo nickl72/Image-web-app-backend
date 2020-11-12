71646ec3821de6722084a90afa923d5437dc9a67
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-12 15:30:58 -0500

    heroku

--- image_backend/settings.py
+++ image_backend/settings.py
@@ -26,7 +26,7 @@ import os
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',            
 'flow-images.herokuapp.com']
