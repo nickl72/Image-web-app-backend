eb711666ffaafa83f04c488cd3f7ce903d065418
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-13 10:35:59 -0500

    stuff

--- image_backend/settings.py
+++ image_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
