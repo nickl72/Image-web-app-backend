935ba75a6124ac8b7971f3cc43ec88abd23892a2
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-16 19:00:05 -0500

    heroku

--- image_backend/settings.py
+++ image_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-# import django_heroku
+import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -188,4 +188,4 @@ SIMPLE_JWT = {
 }
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
